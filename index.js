a1962ef8ed47c32baac3b940624fd240e62849c7
oseji <82390694+oseji@users.noreply.github.com>
2022-12-08 00:19:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const email = document.querySelector(".input-bar");
+const go = document.querySelector(".btn-go");
+const text = document.getElementById("hidden-email");
+console.log(text);
+
+go.addEventListener("click", function () {
+  if (email.value.includes("@")) {
+    console.log("Approved");
+    email.value = "";
+    text.classList.add("hidden");
+  } else {
+    text.classList.toggle("hidden");
+  }
+});

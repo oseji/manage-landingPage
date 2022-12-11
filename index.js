e797c1b267a836b01c72d7e0813fe616174134f5
oseji <82390694+oseji@users.noreply.github.com>
2022-12-11 09:56:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const email = document.querySelector(".input-bar");
 const go = document.querySelector(".btn-go");
 const text = document.getElementById("hidden-email");
 console.log(text);
+const menuToggle = document.querySelector(".nav-hamburger");
+const navList = document.querySelector(".nav-list");
 
+//bottom form submission
 go.addEventListener("click", function () {
   if (email.value.includes("@")) {
     console.log("Approved");
@@ -15,9 +18,7 @@ go.addEventListener("click", function () {
   }
 });
 
-const menuToggle = document.querySelector(".nav-hamburger");
-const navList = document.querySelector(".nav-list");
-
+//nav-menu toggle
 menuToggle.addEventListener("click", function () {
   navList.classList.toggle("hidden");
   console.log("clicked");
